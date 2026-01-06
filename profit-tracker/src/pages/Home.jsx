f9d455d521ffa8b023397fc1838bee2a44f44021
Tom Fijalkowski <tomfijalkowski@yahoo.com>
2026-01-06 18:38:26 -0500

    Initialize year

--- profit-tracker/src/pages/Home.jsx
+++ profit-tracker/src/pages/Home.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+import { Select } from '@mantine/core'
 import StatsGridIcons from "../components/StatGridHome.jsx";
 import MonthlyLineChart from '../components/MonthlyLineChart.jsx'
 
 
+
 function Home() {
+
+    const currentYear = new Date().getFullYear()
+    const [selectedYear, setSelectedYear] = useState(currentYear)
+
+   
+
     return (
         <>
+         <Select
+            placeholder = "Pick a year"
+            data={[currentYear-3, currentYear-2, currentYear-1, currentYear]}
+            onChange={setSelectedYear}
+        />
             <StatsGridIcons />
             <MonthlyLineChart/>
         </>
